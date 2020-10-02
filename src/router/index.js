3c6a8d6fda9105f7595052a2bbd3e0c2bbcd655b
李鑫生 <2272897959@qq.com>
2020-10-03 02:28:34 +0800

    侧边栏退出首页页面实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/Login'
+import Home from '@/views/Home'
+import Welcome from '@/views/Welcome'
 
 Vue.use(VueRouter)
 
@@ -13,6 +15,19 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    redirect: '/welcome',
+    component: Home,
+    children: [
+      {
+        path: '/welcome',
+        name: 'welocme',
+        component: Welcome
+      }
+    ]
   }
 ]
 
